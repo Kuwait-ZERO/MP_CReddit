290ad8ac5484956ce9cda54cb754539610151526
Kuwait-zero <hussein-_2000@hotmail.com>
2024-11-12 03:15:51 -0500

    morgan used

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const postRouter = require("./api/routers");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use("/posts", postRouter);
 const PORT = 8001;
 app.listen(PORT, () => {
